22b1cde5d7bbf642a69aa07edf2d285dee865bfc
Seu Nome <seu-email@example.com>
2024-09-26 12:38:19 -0300

    Dolar Cotação

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+
+#Pegar Infomações Da API
+requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL")
+requisicao_dicionario = requisicao.json()
+cotacao = float(requisicao_dicionario['USDBRL']['bid'])
+print(cotacao)
+
+#Enviar email
+import smtplib
+import email.message
+
+def enviar_email(cotacao):  
+    corpo_email = f"""
+    <p>O valor Do Dolar Está Abaixo de 6$.: R${cotacao}</p>
+    """
+
+    msg = email.message.Message()
+    msg['Subject'] = "Dolar Abaixo De 6$ Reais"
+    msg['From'] = 'rj5206410@gmail.com'
+    msg['To'] = 'jgenese6@gmail.com'
+    password = 'kmtimqitquopuivm' 
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email )
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print('Email enviado')
+
+#Logica Do Sistema
+if cotacao < 6.00:
+    enviar_email(cotacao)
+
